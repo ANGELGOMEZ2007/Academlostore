5a03e296a7969e1642614378c654c711ff977cf7
ANGELGOMEZ2007 <aagc8969@gmail.com>
2023-03-08 16:06:44 -0400

    footer realizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+async function getProducts(){
+    try {
+        const data = await fetch(
+            "https://ecommercebackend.fundamentos-29.repl.co/"
+        );
+
+        const res = await data.json();
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function main() {
+    const res = await getProducts();
+
+    console.log(res);
+}
+
+main()
+
+
 const iconMenu = document.querySelector("#dashboard")
 const menu = document.querySelector("#menu")
 
