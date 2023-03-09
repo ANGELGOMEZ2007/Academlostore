930aac55ae81d8101e677ea39f5691fe4c225cd0
ANGELGOMEZ2007 <aagc8969@gmail.com>
2023-03-09 16:47:31 -0400

    galeria de imagenes y un poco mas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-async function getProducts(){
+async function getProducts() {
     try {
         const data = await fetch(
             "https://ecommercebackend.fundamentos-29.repl.co/"
@@ -6,31 +6,105 @@ async function getProducts(){
 
         const res = await data.json();
 
+        window.localStorage.setItem("products", JSON.stringify(res));
+
         return res;
     } catch (error) {
         console.log(error);
     }
 }
 
-async function main() {
-    const res = await getProducts();
+function printProducts(db) {
+    const productsHTML = document.querySelector(".products");
 
-    console.log(res);
+    let html = "";
+
+    for (const product of db.products) {
+        html += `
+        <div class="product">
+            <div class="product__img">
+                <img src="${product.image}" alt="imagen" />
+            </div>
+
+            
+
+            <div class="product__info">
+                <h4>
+                    $${product.price.toFixed(2)} <span><b>Stock</b>: ${product.quantity}</span> 
+                    <i class='bx bx-plus' id='${product.id}'></i>
+                </h4>
+
+
+                <h3>${product.name}</h3>
+                
+            </div>
+
+        </div>
+        `;
+    }
+
+
+    productsHTML.innerHTML = html;
 }
 
+
+async function main() {
+    const db = {
+        products:
+            JSON.parse(window.localStorage.getItem('products')) ||
+            await getProducts(),
+        cart: {},
+    };
+
+    printProducts(db);
+
+
+    const iconShoppingHTML = document.querySelector('.bx-shopping-bag');
+    const cartHTML = document.querySelector('.cart');
+    const iconXcircle = document.querySelector('.bx-x-circle');
+
+    iconShoppingHTML.addEventListener('click', function(){
+        cartHTML.classList.toggle("cart__show")
+    });
+
+    iconXcircle.addEventListener('click', function() {
+        cartHTML.classList.toggle("cart__show")
+    });
+}
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const iconMenu = document.querySelector("#dashboard")
 const menu = document.querySelector("#menu")
 
-iconMenu.addEventListener('click', function(){
+iconMenu.addEventListener('click', function () {
     menu.classList.toggle("menu__show")
 })
 
 const navbar = document.querySelector("#navbar");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     if (window.scrollY > 250) {
         navbar.classList.add("navbar__active")
     } else {
